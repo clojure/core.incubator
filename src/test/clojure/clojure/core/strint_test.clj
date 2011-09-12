e7627ef0586735271d478b1fe97c2d3c4933e788
Chas Emerick <cemerick@snowtide.com>
2011-09-12 17:05:54 -0400

    migrate clojure.contrib.strint => clojure.core.strint, per http://dev.clojure.org/display/design/Contrib+Library+Names

---
+++ src/test/clojure/clojure/core/strint_test.clj
@@ -0,0 +1,40 @@
+; Copyright (c) Chas Emerick 2009. All rights reserved.
+
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file epl-v10.html at the root of this
+; distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+
+(ns clojure.core.strint-test
+  (:use clojure.test
+        clojure.core.strint))
+      
+(deftest test-silent-read
+  (let [silent-read @#'clojure.core.strint/silent-read]
+    (testing "reading a valid form returns [read form, rest of string]"
+      (is (= [[1] "[2]"] (silent-read "[1][2]"))))
+    (testing "reading an invalid form returns nil"
+      (is (= nil (silent-read "["))))))
+
+(deftest test-interpolate
+  (let [interpolate @#'clojure.core.strint/interpolate]
+    (testing "a plain old string"
+      (is (= ["a plain old string"] (interpolate "a plain old string"))))
+    (testing "some value replacement forms"
+      (is (= '["" foo " and " bar ""] (interpolate "~{foo} and ~{bar}"))))
+    (testing "some fn-calling forms"
+      (is (= '["" (+ 1 2) " and " (vector 3) ""] (interpolate "~(+ 1 2) and ~(vector 3)"))))))
+
+(deftest test-<<
+  (testing "docstring examples"
+    (let [v 30.5
+          m {:a [1 2 3]}]
+      (is (= "This trial required 30.5ml of solution."
+             (<< "This trial required ~{v}ml of solution.")))
+      (is (= "There are 30 days in November."
+             (<< "There are ~(int v) days in November.")))
+      (is (= "The total for your order is $6."
+             (<< "The total for your order is $~(->> m :a (apply +))."))))))
